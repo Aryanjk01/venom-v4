1137405d3c4c64d2bf5c2d52ac3b79bb3f73acf1
Aryanjk01 <arya2051971@gmail.com>
2024-09-01 17:42:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7307293240:AAFbJLiJXeAGr8st9Z9o1ZJB4qinHm56_Y8")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Navu_own")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("7317185449")
 
 
 
